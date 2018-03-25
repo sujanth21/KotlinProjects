b6e139ad3ef5bc0e5edc7122a9e78fdf2c861528
Sujanth <sujanth1987@gmail.com>
2018-03-25 11:03:41 +1100

    Created starter project and methods

--- TimeFighter/app/src/main/java/com/sujanth/timefighter/GameActivity.kt
+++ TimeFighter/app/src/main/java/com/sujanth/timefighter/GameActivity.kt
@@ -1,12 +1,36 @@
 package com.sujanth.timefighter
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
 
 class GameActivity : AppCompatActivity() {
+
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
+    internal lateinit var tapMeButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        //Connect views to variables
+    }
+
+    private fun incrementScore() {
+        //increment score logic
+    }
+
+    private fun resetGame() {
+        //reset game logic
+    }
+
+    private fun startGame() {
+        //start game logic
+    }
+
+    private fun endGame() {
+        //end game logic
     }
 }
